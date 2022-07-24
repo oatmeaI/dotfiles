baafabf984b134cf72e41f2b8f1298fc3aa83a16
Keenan Villani-Holland <keenan.vh@icloud.com>
2022-07-24 15:55:22 -0700

    Update install.sh

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -1,30 +1,33 @@
 #!/bin/zsh
 
-# Oh My ZSH
-# check for installation first
-if ! [ -d "$HOME/.oh-my-zsh" ]; then # mkcd is an alias that omz installs
+# Install Oh My ZSH if we don't have it already
+if ! [ -d "$HOME/.oh-my-zsh" ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
-# Homebrew
-# check for installation first
+# Install Homebrew if we don't have it already
 if ! [ -x "$(command -v brew)" ]; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+# Install Neovim, TheFuck, Yarn, NVM and the Fira Code Nerd Font
+# Brew will exitn with a warning if the package is already installed
 brew install nvim thefuck yarn nvm
 brew tap homebrew/cask-fonts
 brew install font-fira-code-nerd-font
 
+# Brew can't seem to tell if KiTTY is installed already, so we check by hand
 if ! [ -d "/Applications/kitty.app" ]; then
     brew install kitty
 fi
 
+# Install Typewritten shell prompt, if we don't have it yet.
 if ! [ -d "$HOME/.zsh/typewritten" ]; then
     mkdir -p "$HOME/.zsh"
     git clone https://github.com/reobin/typewritten.git "$HOME/.zsh/typewritten"
 fi
 
+# Make backups of everything before pulling all the files down
 if ! [ -d "$HOME/.config-backup" ]; then
     mkdir "$HOME/.config-backup"
 fi
@@ -53,7 +56,8 @@ if [ -f "$HOME/.zshrc" ]; then
     mv "$HOME/.zshrc" "$HOME/.config-backup"
 fi
 
+# Set up the `config` alias, and clone the repo!
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-echo ".dotfiles" >> .gitignore
+echo ".dotfiles" >> .gitignore # This shouldn't be necessary, but just in case.
 git clone --bare https://github.com/oatmeaI/dotfiles.git $HOME/.dotfiles
 config checkout
