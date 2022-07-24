3e6780617d131a6105089477997806d8895afbc4
Keenan Villani-Holland <keenan.vh@icloud.com>
2022-07-24 14:50:16 -0700

    Update install.sh

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -2,7 +2,7 @@
 
 # Oh My ZSH
 # check for installation first
-if ! [ -x "$(command -v mkcd)" ]; then # mkcd is an alias that omz installs
+if ! [ -d "$HOME/.oh-my-zsh" ]; then # mkcd is an alias that omz installs
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
@@ -16,8 +16,30 @@ brew install nvim kitty thefuck yarn nvm
 brew tap homebrew/cask-fonts
 brew install font-fira-code-nerd-font
 
-mkdir -p "$HOME/.zsh"
-git clone https://github.com/reobin/typewritten.git "$HOME/.zsh/typewritten"
+if ! [ -d "$HOME/.zsh/typewritten" ]; then
+    mkdir -p "$HOME/.zsh"
+    git clone https://github.com/reobin/typewritten.git "$HOME/.zsh/typewritten"
+fi
+
+if [ -d "$HOME/.config/kitty" ]; then
+    mv -r "$HOME/.config/kitty" "$HOME/.config-backup/kitty"
+fi
+
+if [ -d "$HOME/.config/nvim" ]; then
+    mv -r "$HOME/.config/nvim" "$HOME/.config-backup/nvim"
+fi
+
+if [ -f "$HOME/.vimrc" ]; then
+    mv "$HOME/.vimrc" "$HOME/.config-backup/.vimrc"
+fi
+
+if [ -f "$HOME/.zprofile" ]; then
+    mv "$HOME/.zprofile" "$HOME/.config-backup/.zprofile"
+fi
+
+if [ -f "$HOME/.zshrc" ]; then
+    mv "$HOME/.zshrc" "$HOME/.config-backup/.zshrc"
+fi
 
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 echo ".dotfiles" >> .gitignore
