772dd5db15cbedf3256532956c22176b36df18da
Keenan Villani-Holland <villanih@adobe.com>
2024-07-26 14:19:26 -0700

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,8 +1,8 @@
 eval "$(/opt/homebrew/bin/brew shellenv)"
 starship init fish | source
 
-set -Ux EDITOR nvim
-set -Ux VISUAL nvim
+set -gx EDITOR nvim
+set -gx VISUAL nvim
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
