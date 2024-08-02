1c08deadcac49b28ae765886dfec3fa2030fb244
Keenan Villani-Holland <villanih@adobe.com>
2024-08-02 09:28:44 -0700

    vim

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -9,5 +9,6 @@ opt.softtabstop = 4 -- 1 tab keypress = 4 spaces
 opt.shiftwidth = 4 -- 1 indent = 4 spaces
 opt.sidescrolloff = 0 -- Don't scroll sideways
 opt.timeoutlen = 250 -- Quicker timeout for which-key etc
--- Always show the tabline
-vim.o.showtabline = 2
+vim.o.updatetime = 250 -- Quicker timeout during key combos etc
+vim.o.showtabline = 2 -- Always show the tabline
+vim.diagnostic.config({ virtual_text = false })
