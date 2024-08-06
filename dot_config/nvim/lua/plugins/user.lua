0b13c7d26d48cb325d0a7b4235e9892d3627a3f3
Keenan Villani-Holland <villanih@adobe.com>
2024-08-06 09:52:30 -0700

    Update .config/nvim/lua/plugins/user.lua

--- dot_config/nvim/lua/plugins/user.lua
+++ dot_config/nvim/lua/plugins/user.lua
@@ -4,6 +4,7 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
+      codelens = { enabled = true },
       diagnostics = { virtual_text = false },
     },
   },
