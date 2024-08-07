e85619bb8280135e0cf5edafcca48a7c84fa6cce
Keenan Villani-Holland <villanih@adobe.com>
2024-08-07 11:57:39 -0700

    update install script

--- install.sh
+++ install.sh
@@ -1,30 +1,51 @@
-#!/bin/sh
+#! /bin/sh
 
-# -e: exit on error
-# -u: exit on unset variables
 set -eu
 
-if ! chezmoi="$(command -v chezmoi)"; then
-    bin_dir="${HOME}/.local/bin"
-    chezmoi="${bin_dir}/chezmoi"
-    echo "Installing chezmoi to '${chezmoi}'" >&2
-    if command -v curl >/dev/null; then
-        chezmoi_install_script="$(curl -fsSL get.chezmoi.io)"
-    elif command -v wget >/dev/null; then
-        chezmoi_install_script="$(wget -qO- get.chezmoi.io)"
-    else
-        echo "To install chezmoi, you must have curl or wget installed." >&2
-        exit 1
-    fi
-    sh -c "${chezmoi_install_script}" -- -b "${bin_dir}"
-    unset chezmoi_install_script bin_dir
+# Install Homebrew
+if ! [ -x "$(command -v brew)" ]; then
+    echo "Homebrew is not installed, installing..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    (
+        echo
+        echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
+    ) >>~/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+else
+    echo "Homebrew is already installed, skipping..."
 fi
 
-# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
-script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+if ! [ -x "$(command -v chezmoi)" ]; then
+    brew install chezmoi
+fi
 
-set -- init --apply "oatmeaI"
+check_app() {
+    if [ -d "/Applications/$1.app" ]; then
+        echo "✅ $1 is already installed."
+        return
+    else
+        echo "‼️ $1 is not installed. Get it here: $2"
+        open "$2"
+    fi
+}
 
-echo "Running 'chezmoi $*'" >&2
-# exec: replace current process with chezmoi
-exec "$chezmoi" "$@"
+# TODO: these won't stay up to date...
+check_app "Alfred 5" "https://cachefly.alfredapp.com/Alfred_5.5_2257.dmg"
+
+check_app "Contexts" "https://contexts.co/releases/Contexts-3.9.0.dmg"
+
+check_app "BatFi" "https://app.gumroad.com/d/9ed109a120b9e2ec59f0dc0c80ccb66f"
+
+check_app "Firefox" "https://www.mozilla.org/firefox/download/thanks/"
+
+check_app "Plexamp" "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-4.11.1-x64.dmg"
+
+check_app "Plex" "https://downloads.plex.tv/plex-desktop/1.98.2.208-0cf517e0/macos/Plex-1.98.2.208-0cf517e0-universal.zip"
+
+check_app "Obsidian" "https://github.com/obsidianmd/obsidian-releases/releases/download/v1.6.7/Obsidian-1.6.7.dmg"
+
+check_app "Anki" "https://github.com/ankitects/anki/releases/download/24.06.3/anki-24.06.3-mac-intel-qt6.dmg"
+
+check_app "HEY" "https://hey-desktop.s3.amazonaws.com/HEY-arm64.dmg"
+
+chezmoi init --apply oatmeaI
