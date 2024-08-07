e2983e0ae180a4edcf45ade224abbd07849c5dbc
Keenan Villani-Holland <villanih@adobe.com>
2024-08-07 11:34:56 -0700

    update install script

--- run_once_before_setup.sh
+++ run_once_before_setup.sh
@@ -30,3 +30,13 @@ check_app "Contexts" "https://contexts.co/releases/Contexts-3.9.0.dmg"
 check_app "BatFi" "https://app.gumroad.com/d/9ed109a120b9e2ec59f0dc0c80ccb66f"
 
 check_app "Firefox" "https://www.mozilla.org/firefox/download/thanks/"
+
+check_app "Plexamp" "https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-4.11.1-x64.dmg"
+
+check_app "Plex" "https://downloads.plex.tv/plex-desktop/1.98.2.208-0cf517e0/macos/Plex-1.98.2.208-0cf517e0-universal.zip"
+
+check_app "Obsidian" "https://github.com/obsidianmd/obsidian-releases/releases/download/v1.6.7/Obsidian-1.6.7.dmg"
+
+check_app "Anki" "https://github.com/ankitects/anki/releases/download/24.06.3/anki-24.06.3-mac-intel-qt6.dmg"
+
+check_app "HEY" "https://hey-desktop.s3.amazonaws.com/HEY-arm64.dmg"
