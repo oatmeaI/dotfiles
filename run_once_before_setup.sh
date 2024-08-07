668c50aba9e0533eb3d28b6fdb07d5bf026fd0e2
Keenan Villani-Holland <villanih@adobe.com>
2024-08-07 11:30:35 -0700

    update install script

--- run_once_before_setup.sh
+++ run_once_before_setup.sh
@@ -4,6 +4,11 @@
 if ! [ -x "$(command -v git)" ]; then
     echo "Homebrew is not installed, installing..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    (
+        echo
+        echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
+    ) >>~/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 else
     echo "Homebrew is already installed, skipping..."
 fi
@@ -14,6 +19,7 @@ check_app() {
         return
     else
         echo "‼️ $1 is not installed. Get it here: $2"
+        open "$2"
     fi
 }
 
@@ -22,3 +28,5 @@ check_app "Alfred 5" "https://cachefly.alfredapp.com/Alfred_5.5_2257.dmg"
 check_app "Contexts" "https://contexts.co/releases/Contexts-3.9.0.dmg"
 
 check_app "BatFi" "https://app.gumroad.com/d/9ed109a120b9e2ec59f0dc0c80ccb66f"
+
+check_app "Firefox" "https://www.mozilla.org/firefox/download/thanks/"
