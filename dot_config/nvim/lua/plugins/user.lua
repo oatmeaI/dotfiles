b3bcec66fd334f9abda19659abf4468e925f8f0a
Keenan Villani-Holland <villanih@adobe.com>
2024-11-22 10:03:14 -0800

    Update .config/nvim/lua/config/keymaps.lua Update .config/nvim/lua/plugins/user.lua

--- dot_config/nvim/lua/plugins/user.lua
+++ dot_config/nvim/lua/plugins/user.lua
@@ -19,21 +19,37 @@ return {
   },
   {
     "folke/snacks.nvim",
-    keys = {
-      term_normal = {
-        "<esc>",
-        function()
-          local in_terminal = vim.bo.buftype == "terminal"
-          if in_terminal then
-            vim.cmd("stopinsert")
-            vim.cmd("hide")
-          end
-        end,
-        mode = "t",
-        expr = true,
-        desc = "close term",
-      },
-    },
+    init = function()
+      Snacks.config.style("terminal", {
+        bo = {
+          filetype = "snacks_terminal",
+        },
+        wo = {},
+        keys = {
+          q = "hide",
+          gf = function(self)
+            local f = vim.fn.findfile(vim.fn.expand("<cfile>"), "**")
+            if f == "" then
+              Snacks.notify.warn("No file under cursor")
+            else
+              self:hide()
+              vim.schedule(function()
+                vim.cmd("e " .. f)
+              end)
+            end
+          end,
+          term_normal = {
+            "<esc>",
+            function(self)
+              vim.cmd("stopinsert")
+            end,
+            mode = "t",
+            expr = true,
+            desc = "Double escape to normal mode",
+          },
+        },
+      })
+    end,
   },
   {
     "akinsho/bufferline.nvim",
