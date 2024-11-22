a6f2ee0c8799482b84adf872b53c9949e9704bb9
Keenan Villani-Holland <villanih@adobe.com>
2024-11-22 10:05:28 -0800

    Update .config/nvim/lua/plugins/user.lua

--- dot_config/nvim/lua/plugins/user.lua
+++ dot_config/nvim/lua/plugins/user.lua
@@ -20,37 +20,13 @@ return {
   {
     "folke/snacks.nvim",
     lazy = false,
-    init = function()
-      Snacks.config.style("terminal", {
-        bo = {
-          filetype = "snacks_terminal",
+    opts = {
+      styles = {
+        terminal = {
+          keys = {},
         },
-        wo = {},
-        keys = {
-          q = "hide",
-          gf = function(self)
-            local f = vim.fn.findfile(vim.fn.expand("<cfile>"), "**")
-            if f == "" then
-              Snacks.notify.warn("No file under cursor")
-            else
-              self:hide()
-              vim.schedule(function()
-                vim.cmd("e " .. f)
-              end)
-            end
-          end,
-          term_normal = {
-            "<esc>",
-            function(self)
-              vim.cmd("stopinsert")
-            end,
-            mode = "t",
-            expr = true,
-            desc = "Double escape to normal mode",
-          },
-        },
-      })
-    end,
+      },
+    },
   },
   {
     "akinsho/bufferline.nvim",
