05028e0a61ee9ca5d5a31673734b578fcb138b7e
Keenan Villani-Holland <villanih@adobe.com>
2025-03-18 16:08:34 -0700

    add venv selector to nvim

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -135,4 +135,20 @@ return {
         dependencies = { "kana/vim-textobj-user" },
     },
     { "willothy/flatten.nvim", config = true, lazy = false, priority = 1001 },
+    {
+        "linux-cultist/venv-selector.nvim",
+        dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+        opts = {
+            -- Your options go here
+            -- name = "venv",
+            -- auto_refresh = false
+        },
+        event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+        keys = {
+            -- Keymap to open VenvSelector to pick a venv.
+            { "<leader>vs", "<cmd>VenvSelect<cr>" },
+            -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+            { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
+        },
+    },
 }
