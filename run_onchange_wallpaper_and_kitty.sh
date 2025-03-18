6f5973d6390a05ed361e6eaa01add422bd40dba2
Keenan Villani-Holland <villanih@adobe.com>
2025-03-18 10:25:26 -0700

    clean up

--- run_onchange_wallpaper_and_kitty.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-osascript -e 'tell application "Finder" to set desktop picture to alias ((path to pictures folder as text) & "wallpaper.png")'
-rm /var/folders/*/*/*/com.apple.dock.iconcache
-killall Dock
