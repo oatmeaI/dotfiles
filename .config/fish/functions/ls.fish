fd7ad7dcaa75d51c04f233f46e0bdb50eafd84bb
Keenan Villani-Holland <villanih@adobe.com>
2025-03-18 10:01:54 -0700

    Switch from chezmoi to yadm

---
+++ .config/fish/functions/ls.fish
@@ -0,0 +1,4 @@
+function ls --wraps='eza -al' --description 'alias ls eza -al'
+  eza -al $argv
+        
+end
---
+++ .config/nvim/lua/plugins/user.lua
@@ -0,0 +1,138 @@
+return {
+
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+                vtsls = {
+                    settings = {
+                        typescript = {
+                            preferences = {
+                                includeCompletionsForModuleExports = true,
+                                includeCompletionsForImportStatements = true,
+                                importModuleSpecifier = "relative",
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    },
+    {
+        "folke/snacks.nvim",
+        lazy = false,
+        opts = {
+            styles = {
+                terminal = {
+                    keys = {
+                        q = "hide",
+                        gf = function(self)
+                            local f = vim.fn.findfile(vim.fn.expand("<cfile>"), "**")
+                            if f == "" then
+                                Snacks.notify.warn("No file under cursor")
+                            else
+                                self:hide()
+                                vim.schedule(function()
+                                    vim.cmd("e " .. f)
+                                end)
+                            end
+                        end,
+                        term_normal = {
+                            "<esc>",
+                            function(self)
+                                vim.cmd("stopinsert")
+                            end,
+                            mode = "t",
+                            expr = true,
+                            desc = "Double escape to normal mode",
+                        },
+                    },
+                },
+            },
+        },
+    },
+    {
+        "akinsho/bufferline.nvim",
+        enabled = false,
+    },
+    {
+        "chrisgrieser/nvim-early-retirement",
+        config = true,
+        event = "VeryLazy",
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            incremental_selection = {
+                keymaps = {
+                    init_selection = "\\",
+                    node_incremental = "\\",
+                },
+            },
+        },
+    },
+    {
+        "folke/flash.nvim",
+        continue = false,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            inlay_hints = { enabled = false },
+            codelens = { enabled = true },
+            diagnostics = { virtual_text = false },
+        },
+    },
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "catppuccin-macchiato",
+        },
+    },
+    { "0xstepit/flow.nvim" },
+    { "rose-pine/neovim" },
+    {
+        "folke/which-key.nvim",
+        opts = {
+            preset = "modern",
+        },
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        opts = {
+            options = {
+                globalstatus = true,
+            },
+            tabline = {
+                lualine_a = { "filename" },
+                lualine_b = { "diagnostics", { sources = { "nvim_lsp" } } },
+                lualine_y = { "tabs" },
+                lualine_z = { "branch" },
+            },
+        },
+    },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        keys = {
+            { "<leader>e", false },
+        },
+        opts = {
+            window = {
+                mappings = {
+                    ["-"] = "navigate_up",
+                },
+            },
+            filesystem = {
+                hijack_netrw_behavior = "open_current",
+                filtered_items = {
+                    visible = true,
+                },
+            },
+        },
+    },
+    {
+        "Julian/vim-textobj-variable-segment",
+        dependencies = { "kana/vim-textobj-user" },
+    },
+    { "willothy/flatten.nvim", config = true, lazy = false, priority = 1001 },
+}
