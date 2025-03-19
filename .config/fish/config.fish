6e68518ed59a729309443914c56e38bffd4d1a5d
Keenan Villani-Holland <keenan@oatmeal.monster>
2025-03-19 14:34:01 -0700

    superfile and bitwarden

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,7 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 starship init fish | source
 
 set -gx EDITOR nvim
+set -gx XDG_CONFIG_HOME "$HOME/.config"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
