ac43ddcc6e65cc50da4995b98abbf62e4fb11802
Keenan Villani-Holland <keenan.vh@icloud.com>
2026-07-21 14:38:15 -0700

    Comment brewup

--- .config/fish/functions/brewup.fish
+++ .config/fish/functions/brewup.fish
@@ -1,8 +1,8 @@
 function brewup  --description 'brew update (itself), upgrade (all packages), cleanup, and dump bundle'
-    og_brew bundle --global
-    og_brew update
-    og_brew upgrade
-    og_brew autoremove
-    og_brew cleanup
-    og_brew bundle dump --global -f --no-npm --no-uv
+    og_brew update                                      # Update Homebrew
+    og_brew bundle --global -f cleanup                  # Delete deps removed from brewfile
+    og_brew bundle --global upgrade                     # Install & upgrade from Brewfile
+    og_brew autoremove                                  # Remove unneeded deps
+    og_brew cleanup                                     # General cleanup
+    og_brew bundle dump --global -f --no-npm --no-uv    # Update Brewfile
 end
