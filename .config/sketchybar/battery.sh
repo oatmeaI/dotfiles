f36728f0c2ebebca3a5c76a83d5d6c4a3c9c11af
Keenan Villani-Holland <keenan.vh@icloud.com>
2026-07-21 12:27:14 -0700

    raycast

--- .config/sketchybar/battery.sh
+++ .config/sketchybar/battery.sh
@@ -2,7 +2,7 @@ INFO=$(pmset -g batt)
 PERCENT=$(pmset -g batt | grep -Eo '\d+%')
 if [[ "$INFO" == *"AC Power"* ]]; then
     # echo ""
-    echo "PWR"
+    echo "PWR: $PERCENT"
 else
     echo "BATT: $PERCENT"
 fi
