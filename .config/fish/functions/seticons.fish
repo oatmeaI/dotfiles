73f7112a42fc2dacce97f5e51aaa24141744d677
Keenan Villani-Holland <keenan@oatmeal.monster>
2026-07-22 13:25:51 -0700

    Make seticons command

---
+++ .config/fish/functions/seticons.fish
@@ -0,0 +1,19 @@
+#!/usr/bin/env fish
+function seticons
+    for file in ~/icons/*
+        if not test -e "$file"
+            continue
+        end
+
+        # Get just the filename (strip directory path)
+        set basename (string replace -r '.*/' '' -- $file)
+
+        # Replace the file extension with .app
+        set newname (string replace -r '\.[^.]+$' '.app' -- $basename)
+
+        set appname "/Applications/$newname"
+
+        sudo fileicon set $appname $file
+    end
+    killall Dock
+end
